39cb1c319432cb21d6c86d127d6f1ca8e1b92175
Steven <steven@ceriously.com>
2020-04-26 00:06:51 -0400

    Add favicons

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,7 +15,14 @@ export default function Layout({ children, title }: Props) {
           <meta charSet="utf-8" />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <title>{title}</title>
-          <link href="/global.css" rel="stylesheet"></link>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+          <link href="/global.css" rel="stylesheet" />
         </Head>
         <Navigation />
         <main className="container">{children}</main>
