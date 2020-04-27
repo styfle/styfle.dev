2818371baf8deab10a2f49df56697cce3730da98
Steven <steven@ceriously.com>
2020-04-26 23:49:35 -0400

    Update green color

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -31,9 +31,9 @@ const Navigation = () => (
 
     <style jsx>{`
       nav {
-        background-color: #00796b;
+        background-color: #184534;
         text-align: center;
-        box-shadow: 0 2px 2px 0 rgba(200,200,200,0.14), 0 3px 1px -2px rgba(200,200,200,0.12), 0 1px 5px 0 rgba(200,200,200,0.2);
+        box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2);
       }
       ul {
         display: flex;
@@ -51,12 +51,11 @@ const Navigation = () => (
       }
       a {
         color: white;
-        text-decoration: none;
         font-size: 20px;
         font-weight: bold;
       }
       a:hover {
-        text-decoration: none !important;
+        border-bottom: none;
       }
     `}</style>
   </nav>
