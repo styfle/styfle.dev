2b79b667d5e81d930a0512031429b7ddfa346cdc
Steven <steven@ceriously.com>
2021-04-11 19:05:36 -0400

    Upgrade highlight.js new API

--- utils/markdown.ts
+++ utils/markdown.ts
@@ -4,11 +4,11 @@ import { highlight, highlightAuto } from 'highlight.js';
 export function markdownToHtml(md: string, baseUrl?: string) {
   const html = marked(md, {
     baseUrl,
-    highlight: (code, lang) => {
-      if (!lang) {
+    highlight: (code, language) => {
+      if (!language) {
         return highlightAuto(code).value;
       }
-      return highlight(lang, code).value;
+      return highlight(code, { language }).value;
     },
   });
   return html;
