d0876c9722e52a3aa6e8d3f5f81e1b3958c27930
Steven <steven@ceriously.com>
2022-05-26 09:39:46 -0400

    Disable support for legacy browsers (#553)
    * Disable support for legacy browsers
    
    * Use browserslist config
    
    * Add supports in front

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
   },
   experimental: {
+    legacyBrowsers: false,
+    browsersListForSwc: true,
     images: {
       layoutRaw: true,
       remotePatterns: [{
