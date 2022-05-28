fcf81b5af0fb5dfa56d31b446bd957589bdee083
Steven <steven@ceriously.com>
2022-05-28 17:27:40 -0400

    Use aboslute url

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,19 +10,16 @@ interface Props {
 }
 
 export default function Layout({ title, ogImage, children }: Props) {
+  const og_image_url = ogImage?.src ?? '/images/blog/ceriously-flat-glow.jpg';
   return (
     <>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="og:title" content={title} />
+        <meta property="og:image" content={og_image_url} />
         <meta name="twitter:creator" content="@styfle" />
-        {ogImage ? (
-          <>
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta property="og:image" content={ogImage.src} />
-          </>
-        ) : null}
+        <meta name="twitter:card" content="summary_large_image" />
         <title>{title}</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
