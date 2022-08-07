0449ae632cfb6b9ddbe0d6fcba1673dae2c7d40e
Steven <steven@ceriously.com>
2022-08-07 18:49:07 -0400

    already

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,16 +5,13 @@ import Footer from './Footer';
 
 interface Props {
   title: string;
+  domain: string;
   ogImage?: { src: string; width: number; height: number } | null;
   children: React.ReactNode;
 }
 
-export default function Layout({ title, ogImage, children }: Props) {
+export default function Layout({ title, domain, ogImage, children }: Props) {
   const imgPath = ogImage?.src ?? '/images/blog/ceriously-flat-glow.jpg';
-  const domain =
-    process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-      ? 'styfle.dev'
-      : process.env.NEXT_PUBLIC_VERCEL_URL;
   return (
     <>
       <Head>
