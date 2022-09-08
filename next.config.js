609555ac463975297060c7e3e21c338e1ba6b3f6
Steven <steven@ceriously.com>
2022-09-08 13:49:31 -0400

    Bump next@12.3.0 (#640)

--- next.config.js
+++ next.config.js
@@ -4,18 +4,15 @@
 const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'via.placeholder.com',
+    }],
   },
   experimental: {
     browsersListForSwc: true,
     legacyBrowsers: false,
-    images: {
-			allowFutureImage: true,
-      remotePatterns: [{
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-      }]
-    }
   }
 }
-  
+
 module.exports = nextConfig
