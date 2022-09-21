8c3173dc276e3390bdddfaadde65dfac2d6824df
Steven <steven@ceriously.com>
2022-09-21 19:39:42 -0400

    Fix config

--- next.config.js
+++ next.config.js
@@ -4,17 +4,14 @@
 const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
+		remotePatterns: [{
+			protocol: 'https',
+			hostname: 'via.placeholder.com',
+		}]
   },
   experimental: {
     browsersListForSwc: true,
-    legacyBrowsers: false,
-    images: {
-			allowFutureImage: true,
-      remotePatterns: [{
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-      }]
-    }
+    legacyBrowsers: false
   }
 }
   
