fb090971eed3c92cde7a642597f6aaa498a89e25
Momin Khan <foolmoron@gmail.com>
2022-10-03 23:26:59 -0400

    Eliminate any by using actual Gtag type

--- utils/gtag.ts
+++ utils/gtag.ts
@@ -1,6 +1,8 @@
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export function pageview(url: string) {
-  const { gtag } = window as any;
+  const { gtag } = window as {
+    gtag: Gtag.Gtag
+  };
   const { GA_ID } = process.env;
   if (GA_ID) {
     gtag('config', GA_ID, {
