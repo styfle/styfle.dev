af82e1bc3cfde40dda7881f80a22eba2282e1b51
Steven <steven@ceriously.com>
2022-11-10 14:01:37 +1000

    Reduce height of simpsons image

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,7 +11,7 @@ export default async function Blog() {
   return (
     <>
       <h1>Blog</h1>
-      <Image src={Simpsons} placeholder="blur" width="720" height="420" alt="Simpsons Any Key" />
+      <Image src={Simpsons} placeholder="blur" width="710" height="360" alt="Simpsons Any Key" />
       {posts.map(({ slug, title, date, content }) => (
         <article key={slug}>
           <h2>
