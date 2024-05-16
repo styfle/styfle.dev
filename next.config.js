e1a2bea7a8a1794d2d04db6e8cf71b5d0cf9c709
Steven <steven@ceriously.com>
2024-05-16 18:17:06 -0400

    chore: experimental react compiler (#816)
    https://react.dev/learn/react-compiler

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [{
