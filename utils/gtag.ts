fa1638ead5c81df52620ddeb31c88e32d5f9784c
Steven <steven@ceriously.com>
2026-04-14 09:41:27 -0400

    fix: next@16.2.3, typesctip@6.0.2, highlight.js@11.11.1 (#858)
    - Closes #857

--- utils/gtag.ts
+++ utils/gtag.ts
@@ -1,7 +1,7 @@
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export function pageview(url: string) {
-  const { gtag } = window as {
-    gtag: Gtag.Gtag;
+  const { gtag } = window as unknown as {
+    gtag: (name: string, id: string, config: { page_path: string }) => void;
   };
   const { GA_ID } = process.env;
   if (GA_ID) {
